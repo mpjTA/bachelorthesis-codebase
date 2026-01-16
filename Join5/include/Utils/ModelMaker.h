ddd9bda1146028233fdd53badd64dfecbb89510d
mpjTA <xz26q4wrfx@privaterelay.appleid.com>
2026-01-16 20:13:41 +0100

    Delete Join5 directory

--- Join5/include/Utils/ModelMaker.h
+++
@@ -1,8 +0,0 @@
-#ifndef MODELMAKER_H
-#define MODELMAKER_H
-
-#include "../Games/Gamestate.h"
-
-ABS::Model* getModel(std::string model_type, const std::vector<std::string>& m_args, int horizon=-1);
-
-#endif //MODELMAKER_H
